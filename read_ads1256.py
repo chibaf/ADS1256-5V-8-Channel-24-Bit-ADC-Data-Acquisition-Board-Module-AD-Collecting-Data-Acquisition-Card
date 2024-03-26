a7effbd0d0a4f2e32b29b61b8173b6e713f2ac37
chibaf <chibaf@mac.com>
2024-03-26 15:00:58 +0900

    Add files via upload

--- read_ads1256.py
+++ read_ads1256.py
@@ -11,8 +11,6 @@ fn="SL_"+str(today)+current_time+".csv"
 f=open(fn,'w',encoding="utf-8")
 start = time.time()
 
-#x=range(0, 100, 1)
-#data=[0]*100
 ser = serial.Serial(sys.argv[1],19200)
 while True:
   try:
@@ -25,20 +23,12 @@ while True:
     line = ser.readline()
     line2=line.strip().decode('utf-8')
     line=[str(val) for val in line2.split(",")]
-#    print(line2)
-#    print(line)
-    str1=st+ss[1:5]+","+str(rttime)+","+str(line[0])+','+str(line[1])+','+str(line[2])+','+str(line[3])+','+str(line[4])+','+str(line[5])+','+str(line[6])+','+str(line[7])+"\n"
+    str1=st+ss[1:5]+", "+str(rttime)+","+str(line[0])+','+str(line[1])+','+str(line[2])+','+str(line[3])+','+str(line[4])+','+str(line[5])+','+str(line[6])+','+str(line[7])+"\n"
     f.write(str1)
     print(str1)
-#    data.pop(-1)
-#    data.insert(0,float(line2[0]))
-#    plt.clf()
-#    plt.ylim([-0.2,1.5])
-#    plt.plot(x,data)
-#    plt.pause(0.1)
   except KeyboardInterrupt:
-    close(ser)
-    close(f)
+    ser.close()
+    f.close()
     print ('exiting')
     break
 exit()
